9612981153bd8c6e16ec5acfc31414575f0382f0
Saiteja <kotagirisaiteja9413@gmail.com>
2024-10-25 10:46:58 +0530

    changes done

--- posts.js
+++ posts.js
@@ -1,10 +1,11 @@
+
 var usersInfo;
 var postsInfo;
 var commentsInfo;
 
 function Data(linkinfo) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', linkinfo, false); 
+    xhr.open('GET', linkinfo, false);
     xhr.send();
 
     if (xhr.status === 200) {
@@ -18,48 +19,76 @@ commentsInfo = Data("https://mocki.io/v1/95131837-44ab-48d1-899f-39b1b594c01b");
 
 function postCard() {
     const container = document.querySelector('.postcontainer');
+    const posterInfo = document.createElement('div');
+    posterInfo.classList.add("card");
+    container.appendChild(posterInfo);
 
     postsInfo.forEach(post => {
         const user = usersInfo.find(user => user.id === post.userId);
-        const posterInfo = document.createElement('div');
-        posterInfo.classList.add("card");
-        posterInfo.innerHTML = `
-            <h3>${user.name}</h3>
+        const postDetails = document.createElement('div');
+        postDetails.classList.add("post");
+        postDetails.innerHTML = `
+            <h4>${user.name}</h4>
             <p>${post.title}</p>
-            <p>~ ${post.body}</p>
+            <p>${post.body}</p>
         `;
-        
-        const button = document.createElement('button');
-        button.classList.add('button');
-        button.textContent = "Comments";
-        let comments = false;
-        button.onclick = function() {
-            comments = !comments; 
-            commented(post, posterInfo, comments);
+
+        const btn = document.createElement('div');
+        btn.classList.add('btn');
+        const commentCard = document.createElement('a');
+        commentCard.classList.add('button');
+        commentCard.textContent = "Comments";
+        commentCard.style.color = 'black';
+        btn.appendChild(commentCard);
+
+    /*    let commentsVisible = false;
+        commentCard.onclick = function() {
+            commentsVisible = !commentsVisible;
+            const Comments = postDetails.querySelectorAll('.comment');
+            Comments.forEach(comment => comment.remove());
+
+            if (commentsVisible) {
+                commented(post, postDetails);
+            }
         };
 
-        posterInfo.appendChild(button);
-        container.appendChild(posterInfo);
+        btn.appendChild(commentCard);*/
+        postDetails.appendChild(btn);
+
+
+        const arrow = document.createElement('i');
+        arrow.classList.add( 'fas', 'fa-angle-down'); 
+        arrow.style.cursor = 'pointer';
+        let commentsVisible = false;
+        arrow.onclick = function() {
+            commentsVisible = !commentsVisible;
+            const Comments = postDetails.querySelectorAll('.comment');
+            Comments.forEach(comment => comment.remove());
+
+            if (commentsVisible) {
+                commented(post, postDetails);
+            }
+        };
+
+
+        btn.appendChild(arrow);
+        posterInfo.appendChild(postDetails);
     });
 }
 
-function commented(post, posterInfo, comments) {
-    const Comments = posterInfo.querySelectorAll('.comment');
-    Comments.forEach(comment => comment.remove());
+function commented(post, postDetails) {
+    commentsInfo.forEach(comment => {
+        if (comment.postId === post.id) {
+            const commentUser = usersInfo.find(user => comment.userId === user.id);
+            const commentBody = document.createElement('div');
+            commentBody.classList.add("comment");
+            commentBody.innerHTML = `
+                <p><b>${commentUser.name}</b></p>
+                <p class="info">${comment.body}</p>`;
+            postDetails.appendChild(commentBody);
+        }
+    });
 
-    if (comments) {
-        commentsInfo.forEach(comment => {
-            if (comment.postId === post.id) {
-                const commentUser = usersInfo.find(user => comment.userId === user.id);
-                const commentBody = document.createElement('div');
-                commentBody.classList.add("comment");
-                commentBody.innerHTML = `
-                    <p>${commentUser.name}</p>
-                    <p class="info">${comment.body}</p>`;
-                posterInfo.appendChild(commentBody);
-            }
-        });
-    }
 }
 
 postCard();
